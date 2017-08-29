03109c038a7daa0728739506c752a3d9776b6e4e
spooniester <spooniester@gmail.com>
2017-08-29 21:33:55 +0200

    Create module.php

---
+++ volumino/module.php
@@ -0,0 +1,40 @@
+<?
+	class Volumio extends IPSModule
+	{
+        
+        var $IP;
+                public function Create()
+                {
+                        //Never delete this line!
+                        parent::Create();
+        
+                        //These lines are parsed on Symcon Startup or Instance creation
+                        //You cannot use variables here. Just static values.
+                        $this->RegisterPropertyString("IP-Adress", "192.168.199.123");
+       
+                }
+		
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+                        $this->IP = $this->ReadPropertyString("IP-Adress");
+                        $this->RegisterVariableBoolean("Volumio_On", "Volumio Server Online");
+			$this->RegisterTimer("GetStatus", 30000, 'Volumio_GetStatus($_IPS[\'TARGET\']);');
+			$this->GetCUPS();
+                      
+			
+		}
+                public function Getstatus()
+                {
+                        $this->IP = $this->ReadPropertyString("IP-Adress");
+                        $URL = "http://" . $this->IP . "/api/v1/getstate";
+                        //$BUFFER = implode(':', file($URL));
+   
+                        //SetValue($this->GetIDForIdent(""), $ERG);
+                        $PING = Sys_Ping($this->IP, 1000);
+                        SetValue($this->GetIDForIdent("Volumio_On"), $PING);
+	
+                }
+        }
+?>
