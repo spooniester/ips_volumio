0f4cd7edf2ad7ac0dea4bf0a4454f8b9868ea11e
spooniester <spooniester@gmail.com>
2017-08-30 15:07:57 +0200

    Update module.php

--- volumino/module.php
+++ volumino/module.php
@@ -11,7 +11,7 @@
                         //These lines are parsed on Symcon Startup or Instance creation
                         //You cannot use variables here. Just static values.
                         $this->RegisterPropertyString("IPAddress", "127.0.0.1");
-       
+       			$this->RegisterPropertyInteger("UpdateInterval", 15);
                 }
 		
 		public function Destroy()
@@ -26,11 +26,21 @@
 			//Never delete this line!
 			parent::ApplyChanges();
                         $this->IP = $this->ReadPropertyString("IPAddress");
-                        $this->RegisterVariableBoolean("Volumio_On", "Volumio Server Online");
+                       online = $this->RegisterVariableBoolean("Volumio_On", "Volumio Server Online");
+			$this->EnableAction("Volumio_On");
 			//$this->RegisterTimer("GetStatus", 30000, 'Volumio_GetStatus($_IPS[\'TARGET\']);');
-                      
+                     $this->RegisterTimer('INTERVAL', $this->ReadPropertyInteger('UpdateInterval'), 'Volumio_GetStatus($id)');
 			
 		}
+		
+		 protected function RegisterTimer($ident, $interval, $script) {
+    $id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+    if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
+      IPS_DeleteEvent($id);
+      $id = 0;
+    }
+			 
+			 
                 public function GetStatus()
                 {
                         $this->IP = $this->ReadPropertyString("IPAddress");
