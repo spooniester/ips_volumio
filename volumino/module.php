3a8c15cc54a95af06fd1270db005da0655d1bbb5
spooniester <spooniester@gmail.com>
2017-08-30 14:18:45 +0200

    Update module.php

--- volumino/module.php
+++ volumino/module.php
@@ -10,7 +10,7 @@
         
                         //These lines are parsed on Symcon Startup or Instance creation
                         //You cannot use variables here. Just static values.
-                        $this->RegisterPropertyString("IP-Address", "192.168.199.123");
+                        $this->RegisterPropertyString("IP-Address", "127.0.0.1");
        
                 }
 		
