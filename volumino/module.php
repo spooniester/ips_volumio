7e6b6eb148c204294960adbc7a1ab202c3f45f0d
spooniester <spooniester@gmail.com>
2017-08-31 20:23:37 +0200

    Update module.php

--- volumino/module.php
+++ volumino/module.php
@@ -68,5 +68,10 @@
                         SetValue($this->GetIDForIdent("Volumio_On"), $PING);
 	
                 }
+		public function volumio_Play()
+		{
+		$this->IP = $this->ReadPropertyString("IPAddress");
+                        $URL = "http://" . $this->IP . "/api/v1/commands/?cmd=play";
+		}
         }
 ?>
