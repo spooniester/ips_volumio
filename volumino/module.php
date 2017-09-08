54daa28a1d80b430c54a1c698a699ab920ca3337
mb <spooniester>
2017-09-08 12:43:00 +0200

    test

--- volumino/module.php
+++ volumino/module.php
@@ -29,13 +29,47 @@
                         $this->IP = $this->ReadPropertyString("IPAddress");
                        $this->ONLINE = $this->RegisterVariableBoolean("Volumio_On", "Volumio Server Online");
 						$this->EnableAction("Volumio_On");
+						//IP Prüfen
+		$ip = $this->ReadPropertyString('IPAddress');
+		if (!filter_var($ip, FILTER_VALIDATE_IP) === false)
+				{
+				$this->SetStatus(102); //IP Adresse ist gültig -> aktiv
+				}
+				$this->RegisterTimer('INTERVAL', $this->ReadPropertyInteger('UpdateInterval'), 'GetStatus($id)');
 		}
+		
+		protected function RegisterTimer($ident, $interval, $script) {
+    $id = @IPS_GetObjectIDByIdent($ident, $this->InstanceID);
+    if ($id && IPS_GetEvent($id)['EventType'] <> 1) {
+      IPS_DeleteEvent($id);
+      $id = 0;
+    }
+    if (!$id) {
+      $id = IPS_CreateEvent(1);
+      IPS_SetParent($id, $this->InstanceID);
+      IPS_SetIdent($id, $ident);
+    }
+    IPS_SetName($id, $ident);
+    IPS_SetHidden($id, true);
+    IPS_SetEventScript($id, "\$id = \$_IPS['TARGET'];\n$script;");
+    if (!IPS_EventExists($id)) throw new Exception("Ident with name $ident is used for wrong object type");
+    if (!($interval > 0)) {
+      IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, 1);
+      IPS_SetEventActive($id, false);
+    } else {
+      IPS_SetEventCyclic($id, 0, 0, 0, 0, 1, $interval);
+      IPS_SetEventActive($id, true);
+    }
+  }
+		
+		
+		
+		
 		public function GetStatus()
                 {
                         $this->IP = $this->ReadPropertyString("IPAddress");
                         $URL = "http://" . $this->IP . ":3000/api/v1/getstate";
    						$BUFFER = implode('', file($URL));
-						//echo $BUFFER;
 						$data = json_decode($BUFFER);
 						var_dump($data);
                         //$PING = Sys_Ping($this->IP, 1000);
